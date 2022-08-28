4f2e9d896e0bc3bcf8e1036fbbb08a5fefad8782
Akilesh Jayaraman <72875302+Akilesh3@users.noreply.github.com>
2022-08-28 22:56:28 +0530

    Add files via upload

---
+++ ipv6_validator.py
@@ -0,0 +1,28 @@
+x = input("Enter the given address ")
+flag = 0
+caps = "ABCDEF"
+lower = caps.lower()
+num = "1234567890"
+characters = caps+lower+num
+splited = x.split(":")
+print(splited)
+if (len(splited) == 8):
+    for i in splited:   
+        if (len(i)==4):
+            for j in i:
+                if j in characters :
+                    flag = 1
+                else:
+                    flag = 0
+                if flag == 0:
+                    print("Its not a valid IPV6 address.")
+                    break
+                else:
+                    continue
+        else:
+            print("Its not a valid IPV6 address.")       
+else:
+    print("Its not a valid IPV6 address.")
+                    
+
+
